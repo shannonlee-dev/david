a4ab0003b1592b07e8756bc34b7f84f42936cf6e
shannonlee-dev <shannon.lee.dev@gmail.com>
2025-08-22 19:38:54 +0900

    solve problem_8

--- devops_1/app.py
+++ devops_1/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import socket
 
 app = Flask(__name__)
 
 @app.route("/menu")
-def show_menu():
-    return render_template("menu.html")
+def home():
+    if app.debug:
+        hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
+    else:
+        hostname = ' '
+    return render_template("menu.html",computername=hostname)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8880)
+    app.run(host='0.0.0.0', port=8800, debug = True)
